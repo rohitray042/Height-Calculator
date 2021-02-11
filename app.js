6e8409e7f287aa0bbb66d8b74463d38695b7d514
Rohit Ray <59976277+rohitray042@users.noreply.github.com>
2021-02-12 01:13:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    let feet = document.querySelector('#feet');
+    let inches = document.querySelector('#inches');
+    const results = document.querySelector('#results');
+    e.preventDefault();
+    feet = parseInt(feet.value);
+    inches = parseInt(inches.value);
+
+    if(isNaN(feet) || isNaN(inches)){
+        results.textContent = "Please Enter a valid number!";
+    }
+    else if(feet < 0){
+        results.textContent = "please Enter a feet value > 0";
+    }
+    else if(inches <0 || inches >=12){
+        results.textContent = "please Enter a inches value  between 0 and 12";
+    }
+    else{
+        let totalInches = parseInt((feet*30.48) + (inches*2.54));
+        
+        results.textContent = `${totalInches} cm`;
+        document.querySelector('#feet').value = '';
+        document.querySelector('#inches').value = '';
+    }
+})
